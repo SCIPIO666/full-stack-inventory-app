c0a8f65b3a97a1b15f66cb959f744b28c9df05d8
SCIPIO666 <eaphoney@gmail.com>
2026-04-06 15:24:17 +0300

    super admin route

--- server/routes/superAdminRoute.js
+++ server/routes/superAdminRoute.js
@@ -2,14 +2,25 @@ const {Router}=require('express')
 
 const superAdminRouter=Router()
 
-superAdminRouter.get('/dashboard')
-superAdminRouter.get('/dashboard/tenants')
-superAdminRouter.get('/dashboard/:tenantId')
-superAdminRouter.get('/dashboard/:tenantId/hospitals')
-superAdminRouter.get('/dashboard/:tenantId/locations')
-superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId')
-superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId/branches')
-superAdminRouter.get('/offboard')
-superAdminRouter.get('/offboard/:tenantId')
+superAdminRouter.get('/dashboard',superAdminController...)
+superAdminRouter.get('/dashboard/tenants',superAdminController...)
+superAdminRouter.get('/dashboard/tenants/offboard',superAdminController...)
+superAdminRouter.get('/dashboard/tenants/:offboardId',superAdminController...)
+superAdminRouter.get('/dashboard/tenants/deactivate',superAdminController...)
+superAdminRouter.get('/dashboard/tenants/deactivate/:id',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/locations',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId/branches',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId/branches/:branchId',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId/branches/:branchId/stock',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId/branches/:branchId/categories',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId/branches/:branchId/departments',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId/branches/:branchId/categories/:categoryId',superAdminController...)
+superAdminRouter.get('/dashboard/:tenantId/hospitals/:hospitalId/branches/:branchId/departments/:departmentId',superAdminController...)
+
+
+module.exports=superAdminRouter;
 
 
